2749ccde7a31ceee3844fa3c549bfee31217699a
cutletmedie <sjkey@mail.ru>
2022-04-28 15:15:05 +0500

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import collections
+
+
+class Snake:
+    def __init__(self, coords):
+        self.coords = coords
+        self.moves = collections.deque()
+
+if __name__ == "__main__":
+    print("hui")
+    pass
