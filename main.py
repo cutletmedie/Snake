e85c438dd4c8c51d4aa33b48eb249bbeed9c6e7a
cutletmedie <sjkey@mail.ru>
2022-05-15 23:17:00 +0500

    Косметические изменения + лист уровней

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pygame.display.set_caption('Snake by Monika Veltman')
 UNIFIED_BUTTONS = ["Выйти в главное меню", "Выйти из игры"]
 MENU_BUTTONS = ["Играть", "Выбрать уровень", UNIFIED_BUTTONS[1]]
 PAUSE_BUTTONS = ["Продолжить"] + UNIFIED_BUTTONS
-PICK_LEVEL_BUTTONS = ["Хуй"] + UNIFIED_BUTTONS
+PICK_LEVEL_BUTTONS = UNIFIED_BUTTONS
 
 black = pygame.Color('black')
 white = pygame.Color('white')
@@ -36,17 +36,23 @@ class Level:
         self.snake = snake
 
 
-levels = {"simple_level":
-              Level(10, Snake([[10 * block_size, 5 * block_size], [9 * block_size, 5 * block_size], [8 * block_size, 5 * block_size], [7 * block_size, 5 * block_size]]))}
+levels = {"first_level":
+              Level(10, Snake([[10 * block_size, 5 * block_size],
+                               [9 * block_size, 5 * block_size],
+                               [8 * block_size, 5 * block_size], [7 * block_size, 5 * block_size]])),
+          "second_level": Level(25, Snake(
+              [[1 * block_size, 3 * block_size], [1 * block_size, 2 * block_size], [1 * block_size, 1 * block_size]]))}
+levels_list = list(levels.keys())
 
 
-class Status:
+class State:
     health_default = 3
     score_default = 0
-    level_default = "simple_level"
+    level_default = levels_list[0]
 
     def __init__(self, picked_level=level_default):
         self.current_level = picked_level
+        self.current_level_index = list(levels.keys()).index(self.current_level)
         self.snake = copy.deepcopy(levels[self.current_level].snake)
         self.score = self.score_default
         self.health = self.health_default
@@ -81,8 +87,10 @@ class Game:
                     elif event.key == pygame.locals.K_RETURN:
                         if list_of_buttons[selected] == 'Играть':
                             self.run()
+                        if list_of_buttons[selected] in levels_list:
+                            self.run(levels_list[selected])
                         if list_of_buttons[selected] == 'Выбрать уровень':
-                            self.mechanism(PICK_LEVEL_BUTTONS, self.draw_pick_level_menu)
+                            self.mechanism(levels_list + PICK_LEVEL_BUTTONS, self.draw_pick_level_menu)
                         if list_of_buttons[selected] == 'Выйти в главное меню':
                             self.mechanism(MENU_BUTTONS, self.draw_main_menu)
                         if selected == len(list_of_buttons) - 1:
@@ -94,19 +102,19 @@ class Game:
         self.draw_general("Snake Game", MENU_BUTTONS, selected)
 
     def draw_pick_level_menu(self, selected=0):
-        self.draw_general("Choose ur (fighter) level", PICK_LEVEL_BUTTONS, selected)
+        self.draw_general("Choose ur (fighter) level", levels_list + PICK_LEVEL_BUTTONS, selected)
 
     def draw_general(self, menu_name, list_of_buttons, selected=0):
         self.surface.fill(white)
         font = pygame.font.SysFont('arial', 80)
-        header = font.render(menu_name, True, (0, 0, 0))
+        header = font.render(menu_name, True, black)
         header_rect = header.get_rect()
         self.surface.blit(header, ((window_x - header_rect.width) / 2, 100))
         button_font = pygame.font.SysFont('arial', 40)
         selected_button_font = pygame.font.SysFont('arial', 45)
         for button in range(len(list_of_buttons)):
             if button == selected:
-                text = selected_button_font.render(list_of_buttons[button], True, (0, 0, 0))
+                text = selected_button_font.render(list_of_buttons[button], True, black)
             else:
                 text = button_font.render(list_of_buttons[button], True, (100, 100, 100))
             self.surface.blit(text, (200, 200 + button * 50))
@@ -116,20 +124,20 @@ class Game:
         rect_object = pygame.Rect(0, window_y, window_x, 150)
         pygame.draw.rect(self.surface, white, rect_object)
         font = pygame.font.SysFont('arial', 30)
-        health = font.render(f"Health: {status.health}", True, (0, 0, 0))
+        health = font.render(f"Health: {status.health}", True, black)
         self.surface.blit(health, (15, window_y + 30))
-        score = font.render(f"Ur mom gay", True, (0, 0, 0))
+        score = font.render(f"Ur mom gay", True, black)
         self.surface.blit(score, (15, window_y + 75))
         pygame.display.update()
 
-    def run(self, picked_level="simple_level"):
+    def run(self, picked_level=levels_list[0]):
         running = True
         pygame.event.clear()
-        status = Status(picked_level)
-        change_to = status.snake.direction
+        state = State(picked_level)
+        change_to = state.snake.direction
 
         while running:
-            self.fps.tick(status.snake.speed)
+            self.fps.tick(state.snake.speed)
             for event in pygame.event.get():
                 if event.type == pygame.locals.QUIT:
                     exit(0)
@@ -142,43 +150,40 @@ class Game:
                         change_to = 'UP'
                     elif event.key in [pygame.locals.K_DOWN, pygame.locals.K_s]:
                         change_to = 'DOWN'
-                if change_to == 'UP' and status.snake.direction != 'DOWN':
-                    status.snake.direction = 'UP'
-                if change_to == 'DOWN' and status.snake.direction != 'UP':
-                    status.snake.direction = 'DOWN'
-                if change_to == 'LEFT' and status.snake.direction != 'RIGHT':
-                    status.snake.direction = 'LEFT'
-                if change_to == 'RIGHT' and status.snake.direction != 'LEFT':
-                    status.snake.direction = 'RIGHT'
-            if status.snake.direction == 'UP':
-                status.snake.position[1] -= block_size
-            elif status.snake.direction == 'DOWN':
-                status.snake.position[1] += block_size
-            elif status.snake.direction == 'LEFT':
-                status.snake.position[0] -= block_size
-            elif status.snake.direction == 'RIGHT':
-                status.snake.position[0] += block_size
+                if change_to == 'UP' and state.snake.direction != 'DOWN':
+                    state.snake.direction = 'UP'
+                if change_to == 'DOWN' and state.snake.direction != 'UP':
+                    state.snake.direction = 'DOWN'
+                if change_to == 'LEFT' and state.snake.direction != 'RIGHT':
+                    state.snake.direction = 'LEFT'
+                if change_to == 'RIGHT' and state.snake.direction != 'LEFT':
+                    state.snake.direction = 'RIGHT'
+            if state.snake.direction == 'UP':
+                state.snake.position[1] -= block_size
+            elif state.snake.direction == 'DOWN':
+                state.snake.position[1] += block_size
+            elif state.snake.direction == 'LEFT':
+                state.snake.position[0] -= block_size
+            elif state.snake.direction == 'RIGHT':
+                state.snake.position[0] += block_size
 
-            print(status.snake.coords, 1)
-            status.snake.coords.insert(0, list(status.snake.position))
-            status.snake.coords.pop()
+            state.snake.coords.insert(0, list(state.snake.position))
+            state.snake.coords.pop()
             self.surface.fill(black)
-            print(status.snake.coords, 2)
 
-            if status.snake.position[0] < 0 or status.snake.position[0] > window_x - block_size \
-                    or status.snake.position[1] < 0 or status.snake.position[1] > window_y - block_size:
-                status.health -= 1
-                status.reset_snake()
-                print(status.snake.coords, 3)
+            if state.snake.position[0] < 0 or state.snake.position[0] > window_x - block_size \
+                    or state.snake.position[1] < 0 or state.snake.position[1] > window_y - block_size:
+                state.health -= 1
+                state.reset_snake()
 
-            for pos in status.snake.coords:
+            for pos in state.snake.coords:
                 pygame.draw.rect(self.surface, white,
                                  pygame.Rect(pos[0], pos[1], block_size, block_size))
 
-            if status.health == 0:
+            if state.health == 0:
                 pygame.time.delay(100)
                 self.mechanism(MENU_BUTTONS, self.draw_main_menu)
-            self.draw_footer(status)
+            self.draw_footer(state)
             pygame.display.update()
 
 
